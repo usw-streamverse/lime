a795a356b0e70e54cbc8b8a39f2cf33b19350954
kimdw524 <kimdw524@naver.com>
2023-04-10 21:49:37 +0900

    Create FormTextBox.tsx

---
+++ src/components/FormTextBox.tsx
@@ -0,0 +1,53 @@
+import { ReactNode, useRef, useState } from 'react';
+import styled, { css } from 'styled-components';
+
+interface FormTextBoxProps {
+    label: string
+}
+
+const FormTextBox = ({label}: FormTextBoxProps) => {
+    const [focus, setFocus] = useState<boolean>(false);
+    const refText = useRef<HTMLInputElement>(null);
+    return (
+        <Wrap focus={focus} onClick={() => refText.current?.focus()}>
+            <Label>{label}</Label>
+            <Input type="text" ref={refText} onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} />
+        </Wrap>
+    )
+}
+
+export default FormTextBox;
+
+const Wrap = styled.div<{focus: Boolean}>`
+    width: 100%;
+    color: #797979;
+    ${(props) => props.focus && css `
+        div {
+            color: var(--main-text-color);
+        }
+        input {
+            border-bottom: 1px solid var(--sign-textbox-border-color-focus);
+        }
+    `}
+`
+
+const Label = styled.div`
+    margin-bottom: 0.75rem;
+    font-size: 0.875rem;
+    letter-spacing: 1px;
+    text-align: left;
+    transition: all 200ms ease;
+`
+
+const Input = styled.input`
+    width: 100%;
+    height: 3.0rem;
+    padding: 0 1.0rem;
+    margin-bottom: 2.0rem;
+    background-color: transparent;
+    border: 0;
+    border-bottom: 1px solid var(--sign-textbox-border-color);
+    color: var(--main-text-color);
+    font-weight: 400;
+    transition: all 200ms ease;
+`;
