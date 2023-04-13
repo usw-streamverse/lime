e57302fe114b647ae8360bfbd23674173e9e995a
kimdw524 <kimdw524@naver.com>
2023-04-13 22:57:21 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import GlobalStyle from './styles/GlobalStyle';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Main from 'pages/Main';
 import NavBar from 'components/NavBar';
+import Upload from 'pages/video/Upload';
+
+const AnimatedSwitch = () => {
+  const location = useLocation();
+  return (
+    <div className="transition-wrap">
+      <TransitionGroup component={null}>
+        <CSSTransition key={location.pathname} classNames="fade" timeout={200}>
+          <div>
+            <Routes location={location}>
+              <Route path="/" element={<Main />} />
+              <Route path="/video/Upload" element={<Upload />} />
+            </Routes>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  )
+}
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <GlobalStyle />
-      <NavBar Content={<Main />} />
-    </>
+      <NavBar>
+        <AnimatedSwitch />
+      </NavBar>
+    </BrowserRouter>
   )
 }
 
