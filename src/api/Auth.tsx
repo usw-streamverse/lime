c3442a57fe152ca3a31250003dd960d735063120
kimdw524 <kimdw524@naver.com>
2023-04-24 00:20:58 +0900

    feat: 로그인, 회원가입 API 추가

--- src/api/Auth.tsx
+++ src/api/Auth.tsx
@@ -1,12 +1,24 @@
 import axios from 'axios';
 
 const Auth = () => {
-    const login = async (id: string, password: string) => {
-        const res = await axios.get('http://localhost:3000/auth');
+    const login = (id: string, password: string) => {
+        const res = axios.post<string>('http://localhost:3000/auth/login', {
+            'id': id,
+            'password': password 
+        });
         return res;
     }
 
-    return { login };
+    const register = (id: string, password: string, nickname: string) => {
+        const res = axios.post<string>('http://localhost:3000/auth/register', {
+            'id': id,
+            'password': password,
+            'nickname': nickname,
+        });
+        return res;
+    }
+
+    return { login, register };
 }
 
 export default Auth;
