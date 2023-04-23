a1791293302b432a349862c1eaff415dcbe97d49
kimdw524 <kimdw524@naver.com>
2023-04-24 00:21:27 +0900

    feat: 로그인 API 적용

--- src/pages/Sign/SignIn.tsx
+++ src/pages/Sign/SignIn.tsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 import { HiOutlineX } from 'react-icons/hi';
-import { useRef, Dispatch, SetStateAction } from 'react';
+import { useRef, Dispatch, SetStateAction, createRef, FormEventHandler } from 'react';
 import FormTextBox from 'components/FormTextBox';
 import CheckBox from 'components/CheckBox';
 import { CSSTransition } from 'react-transition-group';
 import Button from './Button';
+import { useMutation } from '@tanstack/react-query';
+import { Auth } from 'api';
+import { TailSpin } from  'react-loader-spinner'
 
 interface SignInProps {
     setPage: Dispatch<SetStateAction<number>>,
@@ -13,25 +16,55 @@ interface SignInProps {
 }
 
 const SignIn = ({setPage, show, setShow}: SignInProps) => {
+    const id = useRef<HTMLInputElement>(null);
+    const password = useRef<HTMLInputElement>(null);
+    const auth = Auth();
+    const { mutate, status } = useMutation(() => {return auth.login(id?.current?.value || '', password?.current?.value || '')}, {
+        onSuccess: () => {
+            alert('로그인 성공');
+        },
+        onError: () => {
+            alert('로그인 실패');
+        }
+    });
+    
+    const loginAttempt = () => {
+        mutate();
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(status === 'loading') return;
+        loginAttempt();
+    }
+
+    console.log(status);
+
     const nodeRef = useRef<HTMLDivElement>(null);
     return (
         <CSSTransition in={show} nodeRef={nodeRef} timeout={200} classNames="left-swipe" unmountOnExit>
             <Container ref={nodeRef}>
-                <Head>로그인</Head>
-                <Close onClick={() => setShow(false)}><HiOutlineX size={32} /></Close>
-                <FormTextBox label="ID" />
-                <FormTextBox type="password" label="PASSWORD" />
-                <CheckBoxWrapper>
-                    <CheckBox id="auto_login">자동 로그인</CheckBox>
-                </CheckBoxWrapper>
-                <ButtonContainer>
-                    <SignUp onClick={() => setPage(1)}>회원가입</SignUp>
-                    <Button>로그인</Button>
-                </ButtonContainer>
+                <Form onSubmit={handleSubmit}>
+                    <Head>로그인</Head>
+                    <Close onClick={() => setShow(false)}><HiOutlineX size={32} /></Close>
+                    <FormTextBox ref={id} label="ID" />
+                    <FormTextBox ref={password} type="password" label="PASSWORD" />
+                    <CheckBoxWrapper>
+                        <CheckBox id="auto_login">자동 로그인</CheckBox>
+                    </CheckBoxWrapper>
+                    <ButtonContainer>
+                        <SignUp onClick={() => setPage(1)}>회원가입</SignUp>
+                        <Button disabled={status === 'loading'}>{status === 'loading' ? <TailSpin height={24} width={24} color="#fff" visible={true} /> : '로그인'}</Button>
+                    </ButtonContainer>
+                </Form>
             </Container>
         </CSSTransition>
     )
 }
+
+const Form = styled.form`
+    width: 100%;
+`
 
 const Container = styled.div`
     display: flex;
