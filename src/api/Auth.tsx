5bee6c627173609fe700a033f0d5eca8c172562f
kimdw524 <kimdw524@naver.com>
2023-04-25 01:32:54 +0900

    refactor: parameter, response interface

--- src/api/Auth.tsx
+++ src/api/Auth.tsx
@@ -1,20 +1,34 @@
 import axios from 'axios';
 
+export interface LoginParam {
+    id: string,
+    password: string
+}
+
+export interface LoginResult {
+    success: boolean,
+    code: number
+}
+
+export interface RegisterParam {
+    id: string,
+    password: string,
+    nickname: string
+}
+
+export interface RegisterResult {
+    success: boolean,
+    code: number
+}
+
+
 const Auth = () => {
-    const login = (id: string, password: string) => {
-        const res = axios.post<string>('http://localhost:3000/auth/login', {
-            'id': id,
-            'password': password 
-        });
-        return res;
+    const login = (props: LoginParam) => {
+        return axios.post<LoginResult>('http://localhost:3000/auth/login', props);
     }
 
-    const register = (id: string, password: string, nickname: string) => {
-        const res = axios.post<string>('http://localhost:3000/auth/register', {
-            'id': id,
-            'password': password,
-            'nickname': nickname,
-        });
+    const register = (props: RegisterParam) => {
+        const res = axios.post<RegisterResult>('http://localhost:3000/auth/register', props);
         return res;
     }
 
