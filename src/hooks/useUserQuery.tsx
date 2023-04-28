26a678dc642241faf9608ad3a5d502f464e6dd44
kimdw524 <kimdw524@naver.com>
2023-04-29 01:46:01 +0900

    feat: 로그인 상태 업데이트

--- src/hooks/useUserQuery.tsx
+++ src/hooks/useUserQuery.tsx
@@ -1,17 +1,30 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { User } from 'api';
+import { useState } from 'react';
+import { ProfileResult } from 'api/User';
 
 const useUserQuery = () => {
-    const Profile = () => {
-        const data = useQuery({
+    const queryClient = useQueryClient();
+    const [loggedIn, setLoggedIn] = useState<boolean>(false);
+    const Profile = (): [() => void, boolean, ProfileResult | undefined] => {
+        const { data } = useQuery({
             queryKey: ['profile'],
             staleTime: 1000 * 3600,
             retry: false,
-            queryFn: User().profile
+            queryFn: User().profile,
+            onSuccess: (res) => {
+                setLoggedIn(res?.data.success ? true : false);
+            },
+            onError: () => {
+                setLoggedIn(false);
+            }
         });
-        const loggedIn: boolean = data.data?.data.success ? true : false;
 
-        return { loggedIn, ...data.data?.data };
+        const update = () => {
+            queryClient.prefetchQuery(['profile'], User().profile);
+        }
+    
+        return [update, loggedIn, data?.data];
     }
 
     return { Profile }
