8503faffe3f8eafa3ae84be3ef97472a5536df59
kimdw524 <kimdw524@naver.com>
2023-05-14 15:30:34 +0900

    feat: getTimeFormat

--- src/utils/Time.tsx
+++ src/utils/Time.tsx
@@ -1,8 +1,13 @@
-const Time = () => {
-    const getDurationFormat = (time: number): string => {
-        return `${Math.floor(time/60)}:${(time%60<10 ? '0' : '') + time%60}`;
-    }
-    return { getDurationFormat }
+export const getDurationFormat = (time: number): string => {
+    return `${Math.floor(time/60)}:${(time%60<10 ? '0' : '') + time%60}`;
 }
 
-export default Time;
+export const getKSTfromUTC = (time: string): number => {
+    const date = new Date(time);
+    return new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000).getTime();
+}
+
+export const getTimeFormat = (time: number): string => {
+    const date = new Date(time);
+    return `${date.getFullYear()}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + date.getDate()).slice(-2)} ${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}:${('0' + date.getSeconds()).slice(-2)}`;
+}
