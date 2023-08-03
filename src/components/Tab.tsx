a6a6f6b7ece2df8879cbd99e3e1af75a29a2338b
kimdw524 <kimdw524@naver.com>
2023-08-04 03:48:31 +0900

    design: Tab Item hover animation 변경
    hover / 선택된 메뉴의 밑줄이 부드럽게 생김

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -2,12 +2,12 @@ import React, { ReactNode, createContext, useContext } from 'react';
 import styled, { css } from 'styled-components';
 
 interface TabItemProps extends React.DOMAttributes<HTMLDivElement> {
-    name: string
+    index: number
 }
 
-const SelectedContext = createContext<string>('');
+const SelectedContext = createContext<number>(0);
 
-const Tab = (props: {children: ReactNode, selected: string}) => {
+const Tab = (props: {children: ReactNode, selected: number}) => {
     return (
         <SelectedContext.Provider value={props.selected}>
             <Container>
@@ -17,23 +17,52 @@ const Tab = (props: {children: ReactNode, selected: string}) => {
     );
 }
 
-const ItemStyle = styled.div<{selected: boolean}>`
+const ItemStyle = styled.div<{index: number, selected: number}>`
+    position: relative;
     padding: 1.25rem 1.5rem;
     color: var(--main-text-color-light);
     font-weight: 400;
     cursor: pointer;
     transition: all 200ms ease;
+    z-index: 0;
+
     :hover {
         color: var(--main-text-color);
         font-weight: 600;
-        box-shadow: 0 -2px 0 0 var(--main-text-color) inset;
+        ::after {
+            left: 0;
+            right: 0;
+        }
     }
+
+    ::after {
+        position: absolute;
+        ${(props) => 
+            props.selected > props.index ?
+            css `
+                left: 100%;
+                right: 0;
+            ` :
+            css `
+                left: 0;
+                right: 100%;
+            `
+        }
+        bottom: 0;
+        height: 2px;
+        background-color: var(--main-text-color);
+        transition: all 200ms ease;
+        content: '';
+    }
+
     ${(props) => 
-        props.selected &&
+        props.selected === props.index &&
         css `
             color: var(--main-text-color);
             font-weight: 600;
-            box-shadow: 0 -2px 0 0 var(--main-text-color) inset;
+            ::after {
+                right: 0;
+            }
         `
     }
 `
@@ -42,7 +71,7 @@ const Item = (props: TabItemProps) => {
     const selected = useContext(SelectedContext);
 
     return (
-        <ItemStyle selected={props.name === selected} {...props}>{props.children}</ItemStyle>
+        <ItemStyle selected={selected} {...props}>{props.children}</ItemStyle>
     )
 }
 
