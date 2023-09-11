d51f8c6dc587b2d8b46978a0ab1939e9c4d40686
kimdw524 <kimdw524@naver.com>
2023-09-11 20:52:28 +0900

    fix: queryKey 수정

--- src/pages/Live/index.tsx
+++ src/pages/Live/index.tsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 
 const Live = () => {
     const navigate = useNavigate();
-    const list = useQuery(['videoList'], live().list);
+    const list = useQuery(['liveList'], live().list);
     return (
         <Container>
             <Menu>
