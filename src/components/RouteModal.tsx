e6d3f79da00200585fa1fdcf7d2931ec16ab8c1c
kimdw524 <kimdw524@naver.com>
2023-09-14 18:20:29 +0900

    fix: 모바일 해상도에서 최상단에 고정되지 않던 문제 해결

--- src/components/RouteModal.tsx
+++ src/components/RouteModal.tsx
@@ -10,20 +10,20 @@ const RouteModal = (props: {children: ReactNode}) => {
     }, []);
 
     return (
-        <Container animation={animation}>
-            <Wrapper ref={nodeRef} onClick={(e) => e.stopPropagation()}>
-                {props.children}
-            </Wrapper>
-        </Container>
+        <Wrapper animation = {animation} ref={nodeRef} onClick={(e) => e.stopPropagation()}>
+            {props.children}
+        </Wrapper>
     );
 }
 
-const Container = styled.div<{animation: boolean}>`
-    position: absolute;
-    top: 0;
-    left: 0;
+const Wrapper = styled.div<{animation: boolean}>`
+    position: fixed;
+    top: 0; left: 0;
     width: 100%;
     height: 100%;
+    overflow: hidden;
+    z-index: 101;
+    
     ${(props) => props.animation ?
         `
             opacity: 1.0;
@@ -34,21 +34,14 @@ const Container = styled.div<{animation: boolean}>`
             transform: scale(0.9);
         `
     }
-    transition: all 200ms ease;
-`
 
-const Wrapper = styled.div`
-    position: fixed;
-    top: 0; left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    z-index: 101;
     @media screen and (min-width: 481px) {
         position: absolute;
         height: calc(100vh - 4.0rem);
         z-index: 4;
     }
+
+    transition: all 200ms ease;
 `
 
 export default RouteModal;
